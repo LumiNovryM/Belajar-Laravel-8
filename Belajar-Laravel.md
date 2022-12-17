1f83230a9e3e708a4970f2dca0d7ce11b594c0e0
LumiNovryM <luminovrymekel@gmail.com>
2022-12-17 22:31:51 +0700

    Blade Templating Engine

--- Belajar-Laravel.md
+++ Belajar-Laravel.md
@@ -6,3 +6,12 @@
 [Folder-Resources] : Merupakan Folder yang menyimpan Folder Views yang dimana merupakan UI 
 [Folder-Routes] : Merupakan Folder yang berisi semua rute aplikasi
 [Folder-Public] : Merupakan Folder yang berisi asset statis yang bisa diakses oleh user 
+
+# 2.)Blade Templating Engine
+
+[Blade-Templating-Engine] : Adalah fitur Laravel untuk proses templating yang tidak membatasi penggunaan kode PHP biasa dalam tampilan 
+_Fitur Blade_
+[@extends] : Berguna untuk memanggil layouts pada child view
+[@yield] : Berguna untuk memberi tanda bagian yang akan digunakan oleh child view
+[@section] : Berguna untuk membuat bagian pada child yang extend pada layouts
+[@include] : Berguna untuk memanggil baris kode 
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,73 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',[
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about',[
+        "title" => "About",
         "name" => "Lumi Novry M",
         "email" => "luminovrymekel@gmail.com",
         "image" => "lumi.png",
     ]);
 });
 
+
 Route::get('/blog', function () {
-    return view('posts');
+
+    $blog_posts = [
+        [
+        "title" => "Judul Post Pertama",
+        "slug" => "judul-post-pertama",
+        "author" => "Lumi Novry M",
+        "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet iusto sint aliquid, suscipit repellat pariatur repudiandae voluptate quia voluptatibus harum modi, dolore, aperiam dicta quas eligendi? Explicabo, enim velit corporis repudiandae officia maiores consequatur! Ex libero suscipit modi minus aliquid, dicta commodi repellat asperiores dolor. Nesciunt nisi, veniam omnis ullam deserunt tenetur atque incidunt, tempora minima, voluptatibus saepe ea consequatur est magnam dignissimos unde. Impedit est reprehenderit velit officia unde! Eius ab ratione aut perspiciatis quia reiciendis 
+        temporibus exercitationem fugit."
+        ],
+        [
+        "title" => "Judul Post Kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Alianna Sofia",
+        "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem qui quo quas iure dolore nobis suscipit optio dolor iste, at architecto officia repellendus voluptas accusantium explicabo atque nesciunt deleniti ad beatae doloribus sed, odio esse unde natus? Unde error libero numquam assumenda dolorum vel omnis atque, aut voluptate doloremque amet recusandae earum ea! Laborum nobis asperiores adipisci iusto non atque obcaecati dignissimos enim! Eveniet quia odio ad soluta sunt dolor quas saepe. Deleniti asperiores et itaque, ipsam quos autem iusto exercitationem quidem minima reprehenderit ad, doloremque ex debitis quo incidunt accusantium, odit id ratione optio consectetur tempore praesentium placeat facilis."
+        ]
+    ];
+
+    return view('posts',[
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// Halaman Single Post
+Route::get('posts/{slug}', function($slug) {
+
+    $blog_posts = [
+        [
+        "title" => "Judul Post Pertama",
+        "slug" => "judul-post-pertama",
+        "author" => "Lumi Novry M",
+        "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet iusto sint aliquid, suscipit repellat pariatur repudiandae voluptate quia voluptatibus harum modi, dolore, aperiam dicta quas eligendi? Explicabo, enim velit corporis repudiandae officia maiores consequatur! Ex libero suscipit modi minus aliquid, dicta commodi repellat asperiores dolor. Nesciunt nisi, veniam omnis ullam deserunt tenetur atque incidunt, tempora minima, voluptatibus saepe ea consequatur est magnam dignissimos unde. Impedit est reprehenderit velit officia unde! Eius ab ratione aut perspiciatis quia reiciendis 
+        temporibus exercitationem fugit."
+        ],
+        [
+        "title" => "Judul Post Kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Alianna Sofia",
+        "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem qui quo quas iure dolore nobis suscipit optio dolor iste, at architecto officia repellendus voluptas accusantium explicabo atque nesciunt deleniti ad beatae doloribus sed, odio esse unde natus? Unde error libero numquam assumenda dolorum vel omnis atque, aut voluptate doloremque amet recusandae earum ea! Laborum nobis asperiores adipisci iusto non atque obcaecati dignissimos enim! Eveniet quia odio ad soluta sunt dolor quas saepe. Deleniti asperiores et itaque, ipsam quos autem iusto exercitationem quidem minima reprehenderit ad, doloremque ex debitis quo incidunt accusantium, odit id ratione optio consectetur tempore praesentium placeat facilis."
+        ]
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post',[
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
